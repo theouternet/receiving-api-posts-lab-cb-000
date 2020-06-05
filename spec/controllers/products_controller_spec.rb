d2bae7c70ed7d96b40c50162a625806d262a584e
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-05 07:04:22 +0000

    Automatically backed up by Learn

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -12,23 +12,12 @@ RSpec.describe ProductsController, type: :controller do
   describe "POST create" do
 
     it 'creates a new product without redirect' do
-      post :create, product: @product_attributes
-      expect(response).to_not redirect_to(products_path)
-      expect(Product.count).to eq 1
     end
 
   end
 
   describe "GET show" do
-    it 'returns a JSON representation of the product' do
-      product = Product.create(@product_attributes)
-
-      get :show, id: product.id, format: 'json'
-      body = JSON.parse(response.body)
-      expect(body["name"]).to eq product.name
-      expect(body["description"]).to eq product.description
-      expect(body["inventory"]).to eq product.inventory
-      expect(body["created_at"]).to eq nil
+    it 'returns a JSON representation of' do
     end
   end
 
